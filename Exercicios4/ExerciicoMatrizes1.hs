afdcce8954e24ead7a868e9b028b2ef317e6e39d
Tiago Merli <tiagomerli@gmail.com>
2018-04-01 23:49:41 -0300

    Exercícios Matrizes

--- Exercicios4/ExerciicoMatrizes1.hs
+++ Exercicios4/ExerciicoMatrizes1.hs
@@ -1,8 +1,7 @@
 --Exercicio Matrizes 1	
 {--Exercício 01: Faça uma função que gere uma matriz identidade de tamanho n.--}
 
--- Gera matriz identidade de tamanho n
--- quando j for igual a i, retorna a posicao de True (que é igual 1), se forem diferentes, retorna a posicao de False (que é igual a 0)
+	
 matrizIdentidade :: Int -> [[Int]]
 matrizIdentidade n = [ [fromEnum $ i == j | i <- [1..n]] | j <- [1..n]]
 
